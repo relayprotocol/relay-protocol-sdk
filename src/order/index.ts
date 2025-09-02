e8c5ec3abe26537ee4073aaf6d80f79392b96c10
lljxx1 <lljxx1@gmail.com>
2025-09-02 13:13:23 +0800

    feat: add extra data support

--- src/order/index.ts
+++ src/order/index.ts
@@ -6,6 +6,7 @@ import {
   Hex,
   parseAbiParameters,
 } from "viem";
+import * as tronweb from "tronweb";
 
 import {
   ChainIdToVmType,
@@ -243,12 +244,21 @@ export const decodeOrderCall = (call: string, vmType: VmType): DecodedCall => {
   }
 };
 
-type DecodedExtraData = {
+type DecodedExtraDataEtereumVm = {
   vmType: "ethereum-vm";
   extraData: {
     fillContract: string;
   };
 };
+
+type DecodedExtraDataTronVm = {
+  vmType: "tron-vm";
+  extraData: {
+    fillContract: string;
+  };
+};
+
+type DecodedExtraData = DecodedExtraDataEtereumVm | DecodedExtraDataTronVm;
 
 export const encodeOrderExtraData = (extraData: DecodedExtraData): string => {
   switch (extraData.vmType) {
@@ -256,6 +266,13 @@ export const encodeOrderExtraData = (extraData: DecodedExtraData): string => {
       return encodeAbiParameters(
         parseAbiParameters(["(address fillContract)"]),
         [{ fillContract: extraData.extraData.fillContract as Address }]
+      );
+    }
+
+    case "tron-vm": {
+      return tronweb.utils.abi.encodeParams(
+        ['address'], 
+        [extraData.extraData.fillContract]
       );
     }
 
@@ -280,6 +297,24 @@ export const decodeOrderExtraData = (
           vmType: "ethereum-vm",
           extraData: {
             fillContract: result[0].fillContract.toLowerCase(),
+          },
+        };
+      } catch {
+        throw new Error("Failed to decode extra data");
+      }
+    }
+
+    case "tron-vm": {
+      try {
+        const result = tronweb.utils.abi.decodeParams(
+          ['fillContract'],
+          ['address'],
+          extraData,
+        );
+        return {
+          vmType: "tron-vm",
+          extraData: {
+            fillContract: tronweb.utils.address.fromHex(result.fillContract),
           },
         };
       } catch {
